d74fee7eceebe4d7517678d3fa59ecf3757d1745
Aditi Polkam <aditipolkam@gmail.com>
2022-02-08 22:41:21 +0530

    [ADD] compiled sol file

--- web3-py-simple-storage/deploy.py
+++ web3-py-simple-storage/deploy.py
@@ -1,3 +1,23 @@
+import json
+from solcx import compile_standard, install_solc
+
 with open("../SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
-    print(simple_storage_file)
+
+# compile
+install_solc("0.6.0")
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
