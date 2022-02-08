59801d6c19ab0c1cf9f41882ef5a92e2486a705a
Aditi Polkam <aditipolkam@gmail.com>
2022-02-08 11:07:15 +0530

    [ADD] interaction with other contracts deployed

--- FundMe.sol
+++ FundMe.sol
@@ -2,12 +2,50 @@
 
 pragma solidity >0.6.6 <0.9.0;
 
+interface AggregatorV3Interface {
+    function decimals() external view returns (uint8);
+
+    function description() external view returns (string memory);
+
+    function version() external view returns (uint256);
+
+    // getRoundData and latestRoundData should both raise "No data present"
+    // if they do not have data to report, instead of returning unset values
+    // which could be misinterpreted as actual reported values.
+    function getRoundData(uint80 _roundId)
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+}
+
 contract FundMe {
     mapping(address => uint256) public addressToAmountFunded;
 
     function fund() public payable {
         addressToAmountFunded[msg.sender] += msg.value;
+    }
 
-        //ETH to USD conversion
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x9326BFA02ADD2366b30bacB125260Af641031331
+        );
+        return priceFeed.version();
     }
 }
