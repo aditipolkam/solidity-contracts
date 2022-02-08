3f3f6c10b2e8c4be34a7abd21c36cc1218457113
Aditi Polkam <aditipolkam@gmail.com>
2022-02-08 21:45:03 +0530

    [ADD] all funders balance set to 0 after withdraw

--- FundMe.sol
+++ FundMe.sol
@@ -38,18 +38,20 @@ interface AggregatorV3Interface {
 contract FundMe {
     mapping(address => uint256) public addressToAmountFunded;
     address public owner;
+    address[] public funders;
 
     constructor() public {
         owner = msg.sender;
     }
 
     function fund() public payable {
-        uint256 minimumUSD = 50 * (10**18);
+        uint256 minimumUSD = 5 * (10**18);
         require(
             getConversionRate(msg.value) >= minimumUSD,
             "You need to spend more eth."
         );
         addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
     }
 
     function getVersion() public view returns (uint256) {
@@ -86,5 +88,14 @@ contract FundMe {
 
     function withdraw() public payable onlyOwner {
         payable(msg.sender).transfer(address(this).balance);
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
     }
 }
