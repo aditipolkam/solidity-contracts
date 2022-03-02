0c66b6f762befa4a804048cea5e789b98fc9d3e8
Aditi Polkam <aditipolkam@gmail.com>
2022-03-02 22:45:36 +0530

    [ADD] test script to read values from contract

---
+++ brownie-simple-storage/scripts/read_value.py
@@ -0,0 +1,11 @@
+from brownie import SimpleStorage, accounts, config
+
+
+def read_contract():
+    simple_storage = SimpleStorage[-1]  # most recent deployment
+    # print(simple_storage)
+    print(simple_storage.retrieve())
+
+
+def main():
+    read_contract()
