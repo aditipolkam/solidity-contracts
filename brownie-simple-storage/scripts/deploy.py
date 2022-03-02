0a7a84d3a1a67f4cf402883caafbc5cec1961a76
Aditi Polkam <aditipolkam@gmail.com>
2022-03-02 19:34:35 +0530

    [ADD] use functions of contract deployed

--- brownie-simple-storage/scripts/deploy.py
+++ brownie-simple-storage/scripts/deploy.py
@@ -7,7 +7,14 @@ def deploy_simple_storage():
     # account = accounts.add(config["wallets"]["from_key"])  # for environment variable setup
     account = accounts[0]  # default account
     simple_storage = SimpleStorage.deploy({"from": account})
-    print(simple_storage)
+
+    # using the simple storage functions
+    stored_value = simple_storage.retrieve()
+    print("Stored value:", stored_value)
+    transaction = simple_storage.store(42, {"from": account})
+    transaction.wait(1)
+    updated_stored_value = simple_storage.retrieve()
+    print("Updated stored value:", updated_stored_value)
 
 
 def main():
