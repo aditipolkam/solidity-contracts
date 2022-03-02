b0bee2d3e7a0833e8c2355f18b1b43c9e36b055c
Aditi Polkam <aditipolkam@gmail.com>
2022-03-02 19:47:10 +0530

    [MODIFY] test to check updated value

--- brownie-simple-storage/tests/test_simple_storage.py
+++ brownie-simple-storage/tests/test_simple_storage.py
@@ -12,3 +12,16 @@ def test_deploy():
 
     # asserting
     assert starting_value == expected
+
+
+def test_store():
+    # arrange
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from": account})
+
+    # act
+    simple_storage.store(42, {"from": account})
+    expected = 42
+
+    # assert
+    assert simple_storage.retrieve() == expected
