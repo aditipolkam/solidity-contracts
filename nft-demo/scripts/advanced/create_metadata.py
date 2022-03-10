95b05fad110a755b9e343a07b6003480b8c344c1
Aditi Polkam <aditipolkam@gmail.com>
2022-03-10 19:55:02 +0530

    [ADD] ipfs integration

--- nft-demo/scripts/advanced/create_metadata.py
+++ nft-demo/scripts/advanced/create_metadata.py
@@ -2,6 +2,7 @@ from brownie import AdvancedCollectible, network
 from scripts.helpful_scripts import get_breed
 from metadata.sample_metadata import metadata_template
 from pathlib import Path
+import requests
 
 
 def main():
@@ -24,3 +25,20 @@ def main():
             collectible_metadata["name"] = breed
             collectible_metadata["description"] = f"An adorable {breed} pup!"
             print(collectible_metadata)
+            image_filepath = "./img" + breed.lower().replace("_", "-") + ".png"
+            image_uri = upload_to_ipfs(image_filepath)
+            collectible_metadata["image"] = image_uri
+
+
+def upload_to_ipfs(filepath):
+    with Path(filepath).open("rb") as fp:
+        image_binary = fp.read()
+        ipfs_url = "http://127.0.0.1:5001"
+        endpoint = "/api/v0/add"
+        response = requests.post(ipfs_url+endpoint, files ="file": image_binary)
+        ipfs_hash = response.json()["Hash"]
+        # "./img/puppy.png" -> "puppy.png"
+        filename = filepath.split("/")[-1:][0]
+        image_uri = f"https://ipfs.io/ipfs/{ipfs_hash}?filename={filename}"
+        print(image_uri)
+        return image_uri
