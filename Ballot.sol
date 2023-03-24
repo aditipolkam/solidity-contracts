14774e98c1714cf36a2a6f4454075cdec6194164
aditipolkam <aditipolkam@gmail.com>
2023-03-24 19:35:58 +0530

    [CLEANUP] not needed comments

--- Ballot.sol
+++ Ballot.sol
@@ -2,10 +2,6 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-/**
- * @title Ballot
- * @dev Implements voting process along with vote delegation
- */
 contract Ballot {
     struct Voter {
         uint weight; // weight is accumulated by delegation
